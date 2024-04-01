700076f0e6d23873a5bb9a8c5a5514d28ed968b8
Taseen18 <taseen.rahman7@gmail.com>
2024-04-01 21:27:08 +0100

    more reverts in backend

--- backend/community/db_GetPostData.py
+++ backend/community/db_GetPostData.py
@@ -9,4 +9,25 @@ def get_supabase_client() -> Client:
 def fetch_post():
     client = get_supabase_client()
     data = client.table('posts').select('*').execute()
+    print(data)
     return data
+
+def fetch_comments(post_id):
+    client = get_supabase_client()
+    data = client.table('comments').select('*').eq('post_id', post_id).execute()
+    return data
+
+def add_post(post_title, post_content, user_id):
+    client = get_supabase_client()
+    error_message = None
+    response = client.table('posts').insert({
+        'post_title': post_title,
+        'post_content': post_content,
+        'poster_name': user_id,
+        'likes': 0
+    }).execute()
+
+    if not response.data:
+        error_message = "Failed to create post in supabase."
+        if hasattr(response, 'error') and response.error:
+            error_message = response.error.message
